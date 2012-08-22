e8d8cef4e14fa3a83b4cbbd35fc6904be8c7a9ce
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-08-22 05:18:59 +0100

    Vagrant VM configuration.

---
+++ provision.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+sudo apt-get install -y build-essential bison flex unzip libpopt-dev libpopt0 \
+	libnetpbm10 libnetpbm10-dev
+
+cd /vagrant/tools/
+mkdir -p build
+cd build
+
+tar zxf ../k2asm-1.0bRC3.tar.gz
+pushd k2asm-1.0bRC3/
+CPPFLAGS='-fpermissive -include stdlib.h' ./configure
+make
+sudo make install
+popd
+
+unzip ../k2xkernel-0.9.zip
+pushd k2xkernel-0.9/
+make
+popd
+
+unzip ../k2codingtools-1.0.zip
+pushd k2codingtools-1.0/
+make
+sudo make install
+popd
+
+unzip ../k2disktools-0.9.zip
+pushd k2disktools-0.9/
+make
+sudo make install
+popd
+
+unzip ../k2graphictools-0.2.zip
+pushd k2graphictools-0.2/
+make
+sudo make install
+popd
+
+unzip ../k2profiler-1.0.zip
+pushd k2profiler-1.0/
+make
+sudo make install
+popd
